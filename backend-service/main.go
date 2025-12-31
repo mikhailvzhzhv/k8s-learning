20c279a8648a140d5c09a551f3cd2eaa5dc4a471
mikhailvzhzhv <mihaelvozhzhov@gmail.com>
2025-12-31 12:46:54 +0700

    add backend service

---
+++ backend-service/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "pong",
+			"hostname": hostname,
+		})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
+}
